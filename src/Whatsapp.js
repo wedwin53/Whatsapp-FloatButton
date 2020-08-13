6eae7af0cb911d323b2d3d7f52eec06b826fa6b1
Edwin Rebolledo <wedwin53@gmail.com>
2020-08-13 15:05:40 -0600

    phone number for props

--- src/Whatsapp.js
+++ src/Whatsapp.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 
-export default function WhatsApp() {
+export default function WhatsApp({ cellphoneNumber }) {
   return (
     <div>
       <a
         style={{ color: "white" }}
-        href="https://api.whatsapp.com/send?phone=50689781643&text=Hi,%your%20en%20su%20web%20y%20necesito%20consultar%20sobre..."
+        href="https://api.whatsapp.com/send?phone=PHONENUMBERHERE&text=Hi,%your%20en%20su%20web%20y%20necesito%20consultar%20sobre..."
         className="float"
         target="_blank"
         rel="noopener noreferrer"
